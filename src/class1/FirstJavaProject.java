5c01b4b5c1ecdf53368630e80b693bb2928ed310
hiro.mia <hiro.mia@bracits.com>
2023-03-09 00:02:36 +0600

    first commit

---
+++ src/class1/FirstJavaProject.java
@@ -0,0 +1,9 @@
+package class1;
+
+
+public class FirstJavaProject {
+    public static void main(String[] args) {
+        System.out.println("Welcome to SQA Learning Academy");
+
+    }
+}
---
+++ src/class1/SubClass.java
@@ -0,0 +1,6 @@
+package class1;
+
+
+public class SubClass {
+
+}
